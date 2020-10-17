2c4590902615cd21d8ac8baa73e1a15d816bf660
M.Alvi.Azi <61927154+AlvyAzii@users.noreply.github.com>
2020-10-17 11:29:51 +0700

    Add files via upload

---
+++ Praktikum/Pratikum 1/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo "<h3>Nama Saya Muhammad Alvi Azi</h3>";
+		echo "<p>Saya tinggal di Pemalang Randudongkal</p>";
+		echo "Hobi saya Menantang Adrenalin";
+	}
+}
